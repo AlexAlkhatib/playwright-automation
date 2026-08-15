798fbeaa5616ecae237f8022d1d323b122fd3e46
AlexAlkhatib <104989143+AlexAlkhatib@users.noreply.github.com>
2026-08-15 20:26:48 +0200

    browser context and Page fixtures in Playwright  - Example

--- 01. Getting Started with Playwright/tests/UIBasicTest.spec.js
+++ 01. Getting Started with Playwright/tests/UIBasicTest.spec.js
@@ -1,13 +1,20 @@
-// import @playwright/test annotation from playwright module
 const {test} = require('@playwright/test');
 
-// run a new test case
-// test(): takes two arguments (the title of the test and the test function)
-test("First Playwright Test", async () =>
+test("Browser Context Playwright Test", async ({browser}) =>
 {
-    // playwright code 
-    // testing in js is not sequential that's why we need to use the "async" and "await" keywords to wait until the previous set is completed
-    // step 1 - open browser
-    // step 2 - enter username and password
-    // step 3 - click submit button
+    // chrome - options can be proxies, plugins or cookies anything you want to send
+    // open a new browser (define the brwoser type in playwright.config.js)
+    const context = await browser.newContext();
+    
+    // open a new page within the brwoser
+    // create actual page where you enter the url to start automation
+    const page = await context.newPage();
+
+    // go to a url (if we inject page in function's parameter then the creation of context and page will be done explicitly)
+    await page.goto("https://www.google.com/");
+});
+
+test("Page Context Playwright Test", async ({page}) =>
+{
+    await page.goto("https://www.google.com/");
 });
