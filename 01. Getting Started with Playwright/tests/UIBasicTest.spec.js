10c368ed60217cc8c670636af8d533c0963cf3de
AlexAlkhatib <104989143+AlexAlkhatib@users.noreply.github.com>
2026-08-16 16:23:27 +0200

    Importance of Playwright configuration file and its details to run the tests

--- 01. Getting Started with Playwright/tests/UIBasicTest.spec.js
+++ 01. Getting Started with Playwright/tests/UIBasicTest.spec.js
@@ -1,20 +1,28 @@
-const {test} = require('@playwright/test');
+import { test } from '@playwright/test';
 
-test("Browser Context Playwright Test", async ({browser}) =>
-{
-    // chrome - options can be proxies, plugins or cookies anything you want to send
-    // open a new browser (define the brwoser type in playwright.config.js)
+test("Browser Context Playwright Test", async ({ browser }) => {
+    // Browser context options can include proxies, cookies, permissions, etc.
+    // Open a new browser context.
+    // The browser type is defined in playwright.config.js.
     const context = await browser.newContext();
-    
-    // open a new page within the brwoser
-    // create actual page where you enter the url to start automation
+
+    // Open a new page within the browser context.
+    // This is the actual page where the URL will be loaded.
     const page = await context.newPage();
 
-    // go to a url (if we inject page in function's parameter then the creation of context and page will be done explicitly)
+    // Navigate to the specified URL.
+    // Here, the browser context and page are created explicitly.
     await page.goto("https://www.google.com/");
 });
 
-test("Page Context Playwright Test", async ({page}) =>
-{
+test("Page Fixture Playwright Test", async ({ page }) => {
+    // Playwright automatically creates the browser context and page.
     await page.goto("https://www.google.com/");
 });
+
+/**
+ * test.only("Page Fixture Playwright Test", async ({ page }) => {
+    // Playwright automatically creates the browser context and page.
+    await page.goto("https://www.google.com/");
+});
+ */
