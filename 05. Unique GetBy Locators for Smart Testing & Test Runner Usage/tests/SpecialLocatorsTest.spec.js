597104b02890a2393ddbdb89b6286f9c5815cfac
AlexAlkhatib <104989143+AlexAlkhatib@users.noreply.github.com>
2026-08-23 14:10:51 +0200

    Filtering elements with GetByRole,GetByText and perform chaining methods in step

--- 05. Unique GetBy Locators for Smart Testing & Test Runner Usage/tests/SpecialLocatorsTest.spec.js
+++ 05. Unique GetBy Locators for Smart Testing & Test Runner Usage/tests/SpecialLocatorsTest.spec.js
@@ -15,6 +15,26 @@ test("Playwright Special Locators Test", async ({ page }) => {
     // selectOption: only valid if we select an option in <select>
     await page.getByLabel("Gender").selectOption("Female");
 
+    // get the password by placeholder: getByPlaceholder()
+    await page.getByPlaceholder("Password").fill("abc123");
+
+    // get the submit button by role: getByRole() and click the button
+    // filter where the name is "Submit"
+    await page.getByRole("button", {name: "Submit"}).click();
+
+    // get the success message by text: getByText()
+    // expect to be truthy
+    expect(await page.getByText("Success! The Form has been submitted successfully!.").isVisible()).toBeTruthy();
+
+    // go to the shopping page | Shop
+    await page.getByRole("link", {name: "Shop"}).click();
+
+    // locator with tag name and filter attribute
+    // this locator will return 4 cards without filter
+    // get the "Add" button and click it
+    // getbyRole() ne need to use a second argument: {name: "Add "}
+    await page.locator("app-card").filter({hasText: "iphone X"}).getByRole("button").click();
+
     // Pause the page
     await page.pause();
 });
