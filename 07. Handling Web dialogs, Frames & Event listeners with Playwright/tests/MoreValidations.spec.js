63283201f3936514ff5b115216a9b540aefdab9e
AlexAlkhatib <104989143+AlexAlkhatib@users.noreply.github.com>
2026-08-26 19:32:09 +0200

    How to automate JavaScript Alert popups with Playwright

--- 07. Handling Web dialogs, Frames & Event listeners with Playwright/tests/MoreValidations.spec.js
+++ 07. Handling Web dialogs, Frames & Event listeners with Playwright/tests/MoreValidations.spec.js
@@ -15,11 +15,28 @@ test("Popup validations", async ({page}) => {
 
     // locate the hide and show input
     // assert that the text field is visible
-    await expect(page.locator(".displayed-class")).toBeVisible();
+    await expect(page.locator("#displayed-text")).toBeVisible();
 
     // click the hide button ".hide-textbox"
     await page.getByRole("button", {name: "Hide"}).click();
 
     // assert that the text field is hidden
-    await expect(page.locator(".displayed-class")).toBeHidden();
+    await expect(page.locator("#displayed-text")).toBeHidden();
+
+    // page.on() this methods will help to listen to events
+    // for example page.on("close"): this method will be triggered when the browser is closed
+    // when a dialog is triggered perform an action
+    // there are no HTML tags to get the buttons
+    // dialog.accept(): to confirm
+    // dialog.dismiss(): to cancel
+    page.on("dialog", dialog => dialog.accept());
+
+    // click the confirm button that will show the popup
+    await page.locator("#confirmbtn").click();
+
+    // hover an item with mouse using playwright
+    await page.locator("#mousehover").hover();
+
+    // pause the process
+    await page.pause();
 })
