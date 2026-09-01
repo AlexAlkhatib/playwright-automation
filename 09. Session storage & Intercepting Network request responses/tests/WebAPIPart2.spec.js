6d20bfd127194859f0669b00360a9196d3d65609
AlexAlkhatib <104989143+AlexAlkhatib@users.noreply.github.com>
2026-09-01 20:37:32 +0200

    Understand the playwright route method and its parameters in intercepting

--- 09. Session storage & Intercepting Network request responses/tests/WebAPIPart2.spec.js
+++ 09. Session storage & Intercepting Network request responses/tests/WebAPIPart2.spec.js
@@ -5,6 +5,11 @@ const loginPayload = {
     userPassword: "Iamking@000"
 };
 
+const fakePayLoadOrders = { 
+    data: [], 
+    message: "No Orders" 
+};
+
 let token;
 let webContext;
 
@@ -101,6 +106,33 @@ test("Check Page Title", async () => {
     );
 
     await page.close();
+});
+
+//create order is success
+test('Place the order', async () => {
+    // Create page from webContext
+    const page = await webContext.newPage();
+
+    await page.goto(
+        "https://rahulshettyacademy.com/client/"
+    );
+
+    // intercepting response - API response -> { playwright fakeresponse}-> browser -> render data on front end
+    await page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*",
+    async route => {
+        const response = await page.request.fetch(route.request());
+        let body = JSON.stringify(fakePayLoadOrders);
+        route.fulfill(
+        {
+            response,
+            body, 
+        });
+    });
+
+    await page.locator("button[routerlink*='myorders']").click();
+    await page.waitForResponse("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*")
+
+    console.log(await page.locator(".mt-4").textContent());
 });
 
 
