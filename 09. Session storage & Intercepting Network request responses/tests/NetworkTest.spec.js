b9d5e6fd8f6e62af3c3f12b288edbb29cd986834
AlexAlkhatib <104989143+AlexAlkhatib@users.noreply.github.com>
2026-09-02 09:26:03 +0200

    How to intercept Network request calls with Playwright - Demo

---
+++ 09. Session storage & Intercepting Network request responses/tests/NetworkTest.spec.js
@@ -0,0 +1,23 @@
+import { expect, test, request } from '@playwright/test';
+
+test('@QW Security test request intercept', async ({ page }) => {
+
+    // login and reach orders page
+    await page.goto("https://rahulshettyacademy.com/client");
+    await page.locator("#userEmail").fill("anshika@gmail.com");
+    await page.locator("#userPassword").fill("Iamking@000");
+    await page.locator("[value='Login']").click();
+    await page.waitForLoadState('networkidle');
+    await page.locator(".card-body b").first().waitFor();
+
+    // Go to "My orders"
+    await page.locator("button[routerlink*='myorders']").click();
+    // Change the route of the first order with this given new id
+    await page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=*",
+        route => route.continue({ url: 'https://rahulshettyacademy.com/api/ecom/order/get-orders-details?id=621661f884b053f6765465b6' })
+    );
+    // Click the view button
+    await page.locator("button:has-text('View')").first().click();
+    // Expect to see this message
+    await expect(page.locator("p").last()).toHaveText("You are not authorize to view this order");
+});
