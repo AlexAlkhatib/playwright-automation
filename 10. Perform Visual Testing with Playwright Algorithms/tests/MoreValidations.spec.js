e1610ce4fa68f8d35e059d591ebde8d4039cac0d
AlexAlkhatib <104989143+AlexAlkhatib@users.noreply.github.com>
2026-09-03 14:04:46 +0200

    Starter Code & Screenshots

---
+++ 10. Perform Visual Testing with Playwright Algorithms/tests/MoreValidations.spec.js
@@ -0,0 +1,21 @@
+import { expect, test } from '@playwright/test';
+
+test("Screenshot & Visual Comparision", async ({page}) => {
+    // go to the webiste
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+
+    // assert that the text field is visible
+    await expect(page.locator("#displayed-text")).toBeVisible();
+
+    // take a screenshot of the element
+    await page.locator("#displayed-text").screenshot({ path: "partialScreenshot.png" });
+
+    // click the hide button ".hide-textbox"
+    await page.getByRole("button", {name: "Hide"}).click();
+
+    // Take a screenshot of the whole page
+    await page.screenshot({ path: "screenshot.png" });
+
+    // assert that the text field is hidden
+    await expect(page.locator("#displayed-text")).toBeHidden();
+});
