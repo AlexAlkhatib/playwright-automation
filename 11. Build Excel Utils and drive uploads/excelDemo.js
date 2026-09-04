d664141abf4a45405a1c345542986e844b3de446
AlexAlkhatib <104989143+AlexAlkhatib@users.noreply.github.com>
2026-09-04 17:52:23 +0200

    Build Util functions to read and update excel file strategically

--- 11. Build Excel Utils and drive uploads/excelDemo.js
+++ 11. Build Excel Utils and drive uploads/excelDemo.js
@@ -2,6 +2,8 @@
 const ExcelJS = require('exceljs');
 
 async function excelTest() {
+    let output = {row: -1, column: -1};
+
     // create a workbook (object of the class)
     const workbook = new ExcelJS.Workbook();
 
@@ -14,9 +16,18 @@ async function excelTest() {
     // print all rows of the worksheet
     worksheet.eachRow((row, rowNumber) => {
         row.eachCell((cell, colNumber) => {
-            console.log(cell.value);
+            if (cell.value === "Apple") {
+                output.row = rowNumber; // 3
+                output.column = colNumber; // 2
+            }
         })
     });
+    // get the cell (row number, column number)
+    const cell = worksheet.getCell(output.row, output.column);
+    cell.value = "Ananas";
+
+    // save changes in the workbook (we can also override the current Excel file)
+    await workbook.xlsx.writeFile("./newexcel.xlsx");
 }
 
 excelTest();
